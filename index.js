30c23e1c69167c4cb82da0735ad5b988589268fc
Abdurrazaaq Al-Atharee <abdurrazaaqalatharee@gmail.com>
2022-10-04 13:35:49 +0200

    Create index.js
    practiced declaring and calling variables using js

---
+++ index.js
@@ -0,0 +1,14 @@
+let myFirstName = "Abdurrazaaq"
+let myMiddleName = ""
+let myLastName = "Al-Atharee"
+
+let myFullName = myFirstName + myMiddleName + " " + myLastName
+console.log(myFullName)
+
+console.log("Hello, my name is:", myFullName)
+
+let myAge = 18
+let currentYear = 2022
+let birthYear = 2004
+
+console.log(birthYear)
